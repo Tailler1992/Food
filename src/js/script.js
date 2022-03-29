47b31dacfda671bc7d389fdaf44ed53a74cb4c5e
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-03-29 14:22:51 +0500

    implemented cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 import tabs from './modules/tabs';
 import timer from './modules/timer';
 import modal from './modules/modal';
+import cards from './modules/cards';
 
 window.addEventListener('DOMContentLoaded', function () {
     tabs();
     timer();
     modal();
+    cards();
 });
